a9ad600787d9f35ab139ce8d2502ffb6829a9c28
xexpablo <gameprores@gmail.com>
2021-04-07 21:05:21 -0300

    title-change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Easy Plan - Muita putaria num aplicarivo só',
+      title: 'Easy Plan',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.deepOrangeAccent,
