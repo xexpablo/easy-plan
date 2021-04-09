9ff9a7fd2f5ed281bff35d1b6235a73aa3fb9ac5
xexpablo <gameprores@gmail.com>
2021-04-08 21:06:24 -0300

    title changed again

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Easy Plan.',
+      title: 'Easy Plan - Rafão Gay, Vinicius Fudido, Brunão Perdido',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.deepOrangeAccent,
