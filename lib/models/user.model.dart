d5620298e74383726161773f682446633fb4f8be
Lucas Lima <lucas_16_lima@hotmail.com>
2021-05-17 22:31:22 -0300

    users - model

--- lib/models/user.model.dart
+++ lib/models/user.model.dart
@@ -1,1 +1,10 @@
-class Users
+class User {
+  final String idUser;
+  final String username;
+  final String password;
+  final String name;
+  final String lastname;
+
+  const User(
+      {this.idUser, this.username, this.password, this.name, this.lastname});
+}
